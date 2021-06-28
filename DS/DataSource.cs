a78c8f3e541ab14564f4262d5bf3118558a250aa
aviyaalfasi <57313606+aviyaalfasi@users.noreply.github.com>
2021-06-28 12:13:52 +0300

    Add files via upload

---
+++ DS/DataSource.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Collections.Generic;
+using BE;
+
+
+namespace DS
+{
+    public class DataSource
+    {
+
+        static public List<HostingUnit> Units = new List<HostingUnit>();
+        static public List<Order> Orders = new List<Order>();
+        static public List<GuestRequest> Requests = new List<GuestRequest>();
+        static public List<Host> Hosts = new List<Host>();
+
+    }
+}
+ 
